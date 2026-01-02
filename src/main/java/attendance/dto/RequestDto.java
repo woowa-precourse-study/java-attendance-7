2b0b29642ec1b76e4baf184652157ea8ea8b5d24
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-01-02 20:52:43 +0900

    1차 완료(초기화)

--- src/main/java/attendance/dto/RequestDto.java
+++
@@ -1,8 +0,0 @@
-package attendance.dto;
-
-import java.time.LocalDateTime;
-
-public class RequestDto {
-    public record todayAttendance(String Nickname, LocalDateTime today){
-    }
-}
--- src/main/java/attendance/dto/ResponseDto.java
+++
@@ -1,8 +0,0 @@
-package attendance.dto;
-
-import java.util.Date;
-
-public class ResponseDto {
-    public record todayAttendance(Date today, String result){
-    }
-}
